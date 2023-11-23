e725d62e95550bc21aba0e58fcbfe2ebc5e07d72
Yoga <yogabudiman07@gmail.com>
2023-11-23 21:11:26 +0700

    MPA Init

--- public/js/index.js
+++ public/js/index.js
@@ -2,62 +2,6 @@
 import CountryDetail from "./CountryDetail.js";
 
 const countriesContainer = document.querySelector(".countries__container");
-const pathToRegex = (path) =>
-  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
-
-const getParams = function (match) {
-  const values = match.result.slice(1);
-  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
-    (result) => result[1]
-  );
-  console.log(keys);
-
-  return Object.fromEntries(
-    keys.map((key, i) => {
-      return [key, values[1]];
-    })
-  );
-};
-
-const navigateTo = (url) => {
-  history.pushState(null, null, url);
-  router();
-};
-
-const router = async () => {
-  const routes = [
-    { path: "/", view: () => console.log("Testing Root Route") },
-    {
-      path: "/country/:name",
-      view: CountryDetail,
-    },
-  ];
-
-  // Test each route for potential match
-  const potentialMatches = routes.map((route) => {
-    return {
-      route: route,
-      result: location.pathname.match(pathToRegex(route.path)),
-    };
-  });
-
-  let match = potentialMatches.find(
-    (potentialMatch) => potentialMatch.result !== null
-  );
-
-  if (!match) {
-    match = {
-      route: routes[0],
-      isMatch: true,
-    };
-  }
-  console.log(match);
-  const view = new match.route.view(getParams(match));
-
-  document.querySelector("#app").innerHTML = await view.getHTML();
-};
-
-window.addEventListener("popstate", router);
 
 async function getCountries() {
   try {
@@ -114,8 +58,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const observer = new MutationObserver(callback);
   observer.observe(targetNode, config);
-
-  router();
 });
 
 // const themeToggle = document.querySelector(".test");
